f8bfe94bce7d57143930315a8047c70cadd084a3
woosungchu <woosungchu@gmail.com>
2018-01-25 00:07:00 +0900

    record-file-demo

--- www/js/index.js
+++ www/js/index.js
@@ -17,30 +17,21 @@
  * under the License.
  */
 var app = {
+    cnt : 1,
+
     // Application Constructor
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        var counter = document.getElementById('counter');
+
+        counter.addEventListener('click', this.count.bind(this), false);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    count: function() {
+      var counter = document.getElementById('counter');
+      counter.innerText = this.cnt;
+      this.cnt++;
     }
+
 };
 
 app.initialize();
